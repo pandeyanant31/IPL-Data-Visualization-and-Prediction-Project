94df54d213186654b6a4daf50eef9ca0a847973e
pandeyanant31 <anant6p65@gmail.com>
2023-04-08 01:43:05 +0530

    Few Changes to File

--- app.py
+++ app.py
@@ -1,27 +1,20 @@
-import pickle
 import streamlit as st
-teams = [
-    'Sunrisers Hyderabad',
-    'Mumbai Indians',
-    'Royal Challengers Bangalore',
-    'Kolkata Knight Riders',
-    'Kings XI Punjab',
-    'Chennai Super Kings',
-    'Rajasthan Royals',
-    'Delhi Capitals'
-]
-cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
-       'Chandigarh', 'Jaipur', 'Chennai', 'Cape Town', 'Port Elizabeth',
-       'Durban', 'Centurion', 'East London', 'Johannesburg', 'Kimberley',
-       'Bloemfontein', 'Ahmedabad', 'Cuttack', 'Nagpur', 'Dharamsala',
-       'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi',
-       'Sharjah', None, 'Mohali', 'Bengaluru']
-pipe = pickle.load(open('pipe.pkl','rb'))
 
-st.title('IPL Win Predictor')
+st.title('IPL 2022 Visualization and Analysis')
+# def main_page():
+#     st.markdown("# Main page 🎈")
+#     st.sidebar.markdown("# Main page 🎈")
 
-col1,col2=st.beta_columns(2)
-with col1:
-        st.selectbox('Select the batting team',teams)
-with col2:
-        st.selectbox('Select the bowling team',teams)
+# def page2():
+#     st.markdown("# Page 2 ❄️")
+#     st.sidebar.markdown("# Page 2 ❄️")
+
+# def page3():
+#     st.markdown("# Page 3 🎉")
+#     st.sidebar.markdown("# Page 3 🎉")
+
+# page_names_to_funcs = {
+#     "Main Page": main_page,
+#     "Page 2": page2,
+#     "Page 3": page3,
+# }
